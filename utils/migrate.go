26df28703b223ed9633b240d0bc5db7286c471d2
dakasakti <dakasakti.id@gmail.com>
2022-05-29 08:52:31 +0700

    fix config

--- utils/migrate.go
+++ utils/migrate.go
@@ -8,6 +8,6 @@ import (
 
 func Migrate(db *gorm.DB) {
 	db.AutoMigrate(
-		&entities.User{}, &entities.Room{}, &entities.Facility{}, &entities.Amenities{}, &entities.Transaction{}, &entities.Review{}, &entities.District{}, &entities.House{},
+		&entities.User{}, &entities.District{}, &entities.House{}, &entities.Amenities{}, &entities.Room{}, &entities.Facility{}, &entities.Transaction{}, &entities.Review{},
 	)
 }
