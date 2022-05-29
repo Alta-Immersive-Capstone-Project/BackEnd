cb6b83808973f2e0570e620bfa2a5d24784d1753
galihgpr <galihprayogi25@gmail.com>
2022-05-29 11:17:07 +0700

    edit

--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/jinzhu/copier v0.3.5
-	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.7.2
 	github.com/labstack/gommon v0.3.1
 	github.com/stretchr/testify v1.7.1
--- utils/rds/mysql.go
+++ utils/rds/mysql.go
@@ -1,31 +1,26 @@
-package rds
+package utils
 
 import (
+	"fmt"
 	"kost/configs"
 
-	"github.com/labstack/gommon/log"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func InitDB(config *configs.AppConfig) *gorm.DB {
+func NewMysqlGorm(config *configs.AppConfig) *gorm.DB {
 
-	// conString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True",
-	// 	config.Database.Username,
-	// 	config.Database.Password,
-	// 	config.Database.Host,
-	// 	config.Database.Port,
-	// 	config.Database.Name,
-	// )
-	conString := "root@tcp(localhost:3306)/Sewa?charset=utf8mb4&parseTime=True"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
+		config.Database.Username,
+		config.Database.Password,
+		config.Database.Host,
+		config.Database.Port,
+		config.Database.Name,
+	)
 
-	db, err := gorm.Open(mysql.Open(conString), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err.Error())
+		panic(err.Error())
 	}
 	return db
-}
-
-func Migrate(db *gorm.DB) {
-	db.AutoMigrate()
 }
