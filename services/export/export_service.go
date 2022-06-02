f8f69849e269b4b7b579cf42945a03a10916794d
galihgpr <galihprayogi25@gmail.com>
2022-06-03 06:48:48 +0700

    Pull Main

--- services/export/export_service.go
+++ services/export/export_service.go
@@ -1,12 +1,8 @@
 package export
 
 import (
-	"encoding/csv"
 	"kost/repositories/export"
 	"kost/utils/s3"
-	"os"
-
-	"github.com/labstack/gommon/log"
 )
 
 type ServiceExport struct {
@@ -21,21 +17,21 @@ func NewServiceExport(Repo export.RepoExport, S3 s3.S3Control) *ServiceExport {
 	}
 }
 
-func (s *ServiceExport) CreateFileReport(time int) error {
-	data, err := s.repo.ReportTransaction(7)
-	if err != nil {
-		log.Warn(err)
-		return err
-	}
-	csvFile, err := os.Create("report.csv")
-	if err != nil {
-		log.Warn("failed creating file: %s", err)
-	}
-	csvwriter := csv.NewWriter(csvFile)
+// func (s *ServiceExport) CreateFileReport(time int) error {
+// 	data, err := s.repo.ReportTransaction(7)
+// 	if err != nil {
+// 		log.Warn(err)
+// 		return err
+// 	}
+// 	csvFile, err := os.Create("report.csv")
+// 	if err != nil {
+// 		log.Warn("failed creating file: %s", err)
+// 	}
+// 	csvwriter := csv.NewWriter(csvFile)
 
-	for _, empRow := range data {
-		_ = csvwriter.Write(empRow)
-	}
-	csvwriter.Flush()
-	csvFile.Close()
-}
+// 	for _, empRow := range data {
+// 		_ = csvwriter.Write(empRow)
+// 	}
+// 	csvwriter.Flush()
+// 	csvFile.Close()
+// }
