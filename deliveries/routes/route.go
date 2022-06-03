2c2a9ecbd1e2a51039faad1c902e094db09c2f5b
galihgpr <galihprayogi25@gmail.com>
2022-06-04 04:18:27 +0700

    Edit Route

--- deliveries/routes/route.go
+++ deliveries/routes/route.go
@@ -101,9 +101,9 @@ func ReviewsPath(e *echo.Echo, review review.ReviewHandler) {
 }
 
 func ReminderPath(e *echo.Echo, reminder reminder.ReminderHandler) {
-	e.GET("/auth/google/login", reminder.OauthLogin())
+	e.POST("/auth/google/login", reminder.OauthLogin())
 	e.GET("/auth/google/callback", reminder.OauthCallback())
-	e.GET("/create-reminder", reminder.CreateReminder())
+	e.POST("/create-reminder", reminder.CreateReminder())
 }
 
 func TransactionPath(e *echo.Echo, transaction transaction.TransactionHandler) {
