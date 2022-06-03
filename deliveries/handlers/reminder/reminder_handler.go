7a81a16a15dfd5aa4d316f3cefb221a66d507e95
adeeplearn <galang8280@students.amikom.ac.id>
2022-06-03 15:39:08 +0700

    working reminder save token to cookie

--- deliveries/handlers/reminder/reminder_handler.go
+++ deliveries/handlers/reminder/reminder_handler.go
@@ -1,7 +1,6 @@
 package reminder
 
 import (
-	"context"
 	"fmt"
 	"kost/deliveries/helpers"
 	"kost/entities"
@@ -11,18 +10,15 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
-	"golang.org/x/oauth2"
-	"google.golang.org/api/calendar/v3"
-	"google.golang.org/api/option"
 )
 
 type HandlerReminder struct {
 	auth reminder.ReminderService
 }
 
-var (
-	config *oauth2.Config
-)
+// var (
+// 	config *oauth2.Config
+// )
 
 func NewHandlersReminder(auth reminder.ReminderService) *HandlerReminder {
 	return &HandlerReminder{
@@ -34,24 +30,24 @@ func (h *HandlerReminder) OauthLogin() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		cookie := http.Cookie{Name: "oauthstate", Value: "token-state", Expires: time.Now().Add(365 * 24 * time.Hour)}
 		c.SetCookie(&cookie)
-		authUrl := h.auth.GetLoginUrl(cookie.Value)
 
+		authUrl := h.auth.GetLoginUrl(cookie.Value)
 		return c.Redirect(http.StatusTemporaryRedirect, authUrl)
 	}
 }
 
 func (h *HandlerReminder) OauthCallback() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		oauthState, _ := c.Request().Cookie("oauthstate")
+		oauthState, err := c.Request().Cookie("oauthstate")
 		if c.QueryParam("state") != oauthState.Value {
 			log.Warn("invalid oauth google state")
-			return c.Redirect(http.StatusTemporaryRedirect, "/")
+			return err
 		}
 
-		data, err := h.auth.GetUserDataFromGoogle(c.QueryParam("code"))
+		data, err := h.auth.GetUserDataFromGoogle(c.Response().Writer, c.QueryParam("code"))
 		if err != nil {
 			log.Warn(err)
-			return c.Redirect(http.StatusTemporaryRedirect, "/")
+			fmt.Println(data)
 		}
 
 		fmt.Fprintf(c.Response().Writer, "UserInfo: %s\n", data)
@@ -67,38 +63,18 @@ func (h *HandlerReminder) CreateReminder() echo.HandlerFunc {
 			return c.JSON(http.StatusInternalServerError, helpers.InternalServerError())
 		}
 
-		ctx := context.Background()
-		code := c.QueryParam("code")
-		token, err := config.Exchange(ctx, code)
+		code := c.QueryParams().Get("code")
+		oauthCode := http.Cookie{Name: "oauthcode", Value: code, Expires: time.Now().Add(2 * time.Hour)}
+		c.SetCookie(&oauthCode)
+
+		var request entities.Reminder
+		err := c.Bind(&request)
 		if err != nil {
 			log.Warn(err)
-			return c.JSON(http.StatusInternalServerError, helpers.InternalServerError())
+			return c.JSON(http.StatusBadRequest, helpers.StatusBadRequestBind(err))
 		}
 
-		oauthCode := new(http.Cookie)
-		oauthCode.Name = "oauthtoken"
-		oauthCode.Value = code
-		c.SetCookie(oauthCode)
-
-		client := config.Client(ctx, token)
-		service, err := calendar.NewService(ctx, option.WithHTTPClient(client))
-		if err != nil {
-			log.Warn(err)
-			return c.JSON(http.StatusInternalServerError, helpers.InternalServerError())
-		}
-
-		request := entities.Reminder{
-			Title:       "Coba Create Event",
-			Description: "test desc create event",
-			Year:        "2022",
-			Month:       "06",
-			Day:         "03",
-			Start:       "11:00",
-			End:         "17:00",
-			Attendees:   "gadipuranto@gmail.com",
-		}
-		event := h.auth.CreateEvent(&request)
-		remind, err := service.Events.Insert("primary", event).Do()
+		remind, err := h.auth.CreateEvent(c.QueryParam("code"), request)
 		if err != nil {
 			log.Warn(err)
 			return c.JSON(http.StatusInternalServerError, helpers.InternalServerError())
