1e358ff8426ae69c326749f55ebcbff1642a4c97
Astuti <100201094+astutirahmawati@users.noreply.github.com>
2022-06-06 15:48:34 +0700

    Update general.go

--- deliveries/middlewares/general.go
+++ deliveries/middlewares/general.go
@@ -12,9 +12,4 @@ func General(e *echo.Echo) {
 	}))
 
 	e.Use(middleware.CORS())
-	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	// 	AllowOrigins: []string{"*"},
-	// 	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
-	// 	AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.PATCH, echo.OPTIONS},
-	// }))
 }
